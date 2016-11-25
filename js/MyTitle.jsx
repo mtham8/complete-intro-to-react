bf87f57b9e079bca6fc3d6e3a4746236da54a436
Michelle Tham <mtham101@gmail.com>
2016-11-24 22:49:21 -0800

    Refactor using .jsx

--- js/MyTitle.jsx
+++ js/MyTitle.jsx
@@ -8,9 +8,11 @@ const h1 = React.DOM.h1
 const MyTitle = React.createClass({
   render () {
     return (
-      div(null,
-        h1({ style: { color: this.props.color }}, this.props.title)
-      )
+      <div>
+        <h1 style={{color: this.props.color}}>
+          {this.props.title}
+        </h1>
+      </div>
     )
   }
 })
