49b9cbf1c64c4a792b770b5d7728b10b7ab5a888
Michelle Tham <mtham101@gmail.com>
2016-11-24 23:45:16 -0800

    Add ES Lint to Standard

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -3,13 +3,13 @@ const ReactDOM = require('react-dom')
 
 // you need div to be a parent element for the instances of your class
 const App = () => (
-    <div className='app-container'>
-      <div className='home-info'>
-        <h1 className='title'>V1d305</h1>
-        <input className='search' type='text' placeholder='Search' />
-        <button className='browse-all'> or Browse All </button>
-      </div>
+  <div className='app-container'>
+    <div className='home-info'>
+      <h1 className='title'>V1d305</h1>
+      <input className='search' type='text' placeholder='Search' />
+      <button className='browse-all'> or Browse All </button>
     </div>
+  </div>
 )
 
 ReactDOM.render(<App />, document.getElementById('app'))
