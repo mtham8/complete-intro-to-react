41d7ee81fafdc2616b87e15600a3862e52df1e65
Michelle Tham <mtham101@gmail.com>
2016-11-28 15:57:36 -0800

    Display search results

--- js/Landing.jsx
+++ js/Landing.jsx
@@ -1,4 +1,5 @@
 const React = require('react')
+const { Link } = require('react-router')
 
 // you need div to be a parent element for the instances of your class
 const Landing = () => (
@@ -6,7 +7,7 @@ const Landing = () => (
     <div className='home-info'>
       <h1 className='title'>V1d305</h1>
       <input className='search' type='text' placeholder='Search' />
-      <button className='browse-all'> or Browse All </button>
+      <Link to='/search' className='browse-all'> or Browse All </Link>
     </div>
   </div>
 )
