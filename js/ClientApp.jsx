e2d51a61b6af29c9220d743c322939d4ee18faf9
Michelle Tham <mtham101@gmail.com>
2016-11-28 15:04:12 -0800

    Add json data

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,12 +1,14 @@
 const React = require('react')
 const ReactDOM = require('react-dom')
 const Landing = require('./Landing')
+const Search = require('./Search')
 const { Router, Route, hashHistory } = require('react-router')
 
 // you need div to be a parent element for the instances of your class
 const App = () => (
   <Router history={hashHistory}>
     <Route path='/' component={Landing} />
+    <Route path='/search' component={Search} />
   </Router>
 )
 
