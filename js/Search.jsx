cfff0a2681f271df5c939e15d4255a4fb8c2f451
Michelle Tham <mtham101@gmail.com>
2016-11-28 16:10:26 -0800

    Implement ShowCard component

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,18 +1,13 @@
 const React = require('react')
+const ShowCard = require('./ShowCard')
 const data = require('../public/data.json')
 
 const Search = () => (
   <div className='container'>
     <div className='shows'>
       {data.shows.map(show => (
-        <div className='show-card'>
-          <img src={`public/img/posters/${show.poster}`} className='show-card-img' />
-          <div className='show-card-text'>
-            <h3 className='show-card-title'>{show.title}</h3>
-            <h4 className='show-card-year'>({show.year})</h4>
-            <p className='show-card-description'>{show.description}</p>
-          </div>
-        </div>
+        // show={show} ==> show= is the name of the property we're passing it down as
+        <ShowCard show={show} key={show.imdbID}/>
       ))}
     </div>
   </div>
