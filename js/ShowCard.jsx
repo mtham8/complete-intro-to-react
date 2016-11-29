0b6e4d00c845a03f8e810383880c2427c1f1680a
Michelle Tham <mtham101@gmail.com>
2016-11-28 16:24:37 -0800

    Implement ShowCard component pt 2

--- js/ShowCard.jsx
+++ js/ShowCard.jsx
@@ -1,14 +1,23 @@
 const React = require('react')
 
-const ShowCard = ({ show }) => (
+const ShowCard = (props) => (
   <div className='show-card'>
-    <img src={`public/img/posters/${show.poster}`} className='show-card-img' />
+    <img src={`public/img/posters/${props.poster}`} className='show-card-img' />
     <div className='show-card-text'>
-      <h3 className='show-card-title'>{show.title}</h3>
-      <h4 className='show-card-year'>({show.year})</h4>
-      <p className='show-card-description'>{show.description}</p>
+      <h3 className='show-card-title'>{props.title}</h3>
+      <h4 className='show-card-year'>({props.year})</h4>
+      <p className='show-card-description'>{props.description}</p>
     </div>
   </div>
 )
+
+const { string } = React.PropTypes.isRequired
+// makes sure that props is passed as an object
+ShowCard.propTypes = {
+  title: string,
+  year: string,
+  poster: string,
+  description: string
+}
 
 module.exports = ShowCard
