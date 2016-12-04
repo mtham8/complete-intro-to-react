d22d237684b59de2e7a7e26e56b45f4564166a95
Michelle Tham <mtham101@gmail.com>
2016-12-04 14:04:53 -0800

    Implement this.assignShow in ClientApp.jsx

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -8,14 +8,28 @@ const { Router, Route, IndexRoute, hashHistory } = require('react-router')
 const { shows } = require('../public/data.json')
 
 // you need div to be a parent element for the instances of your class
-const App = () => (
-  <Router history={hashHistory}>
-    <Route path='/' component={Layout}>
-      <IndexRoute component={Landing} />
-      <Route path='/search' component={Search} shows={shows} />
-      <Route path='/details/:id' component={Details} />
-    </Route>
-  </Router>
-)
+const App = React.createClass({
+  assignShow (nextState, replace) {
+    const showArray = shows.filter((show) => show.imdbID === nextState.params.id)
+    // console.log('nextState ==> ', nextState, 'showArray ==> ', showArray)
+    if (showArray.length < 1) {
+      return replace('/')
+    }
+    Object.assign(nextState.params, showArray[0])
+    return nextState
+  },
+  render () {
+    return (
+      <Router history={hashHistory}>
+        <Route path='/' component={Layout}>
+          <IndexRoute component={Landing} />
+          <Route path='/search' component={Search} shows={shows} />
+          {/* every time you go to Details route, it will first run this.assignShow */}
+          <Route path='/details/:id' component={Details} onEnter={this.assignShow} />
+        </Route>
+      </Router>
+    )
+  }
+})
 
 ReactDOM.render(<App />, document.getElementById('app'))
